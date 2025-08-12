9e192129e44ca16768938c0c135877f6bbe13e20
jorawaraksk <jorawarzu@proton.me>
2025-08-12 16:37:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,20 +9,18 @@ AUDIO_FORMAT = "mp3"
 AUDIO_CHANNELS = 1     
 AUDIO_SAMPLE_RATE = 44100  
 
-# Video compression settings
-VIDEO_SCALE = "min(1920,iw):min(1080,ih)"  
-VIDEO_FPS = 24  
+# Video compression settings  
+VIDEO_FPS = 22  
 VIDEO_CODEC = "libx265"  
 VIDEO_BITRATE = "100k"  
 VIDEO_CRF = 30  
-VIDEO_PRESET = "ultrafast"  
+VIDEO_PRESET = "veryfast"  
 VIDEO_PIXEL_FORMAT = "yuv420p"  
 VIDEO_PROFILE = "main"  
 VIDEO_AUDIO_CODEC = "aac"  
 VIDEO_AUDIO_BITRATE = "64k"  
 VIDEO_AUDIO_CHANNELS = 1 
 VIDEO_AUDIO_SAMPLE_RATE = 44100  
-VIDEO_SCALE = '640:360' 
 
 # Temporary file settings
 TEMP_FILE_SUFFIX_AUDIO = ".mp3"  
